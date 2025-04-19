4a80e24e8411ec2433c4d451dfdb9b734a19afd6
Brian <brian.sila1@student.moringaschool.com>
2025-04-19 11:23:09 +0300

    final commit

---
+++ src/pages/UserProfile.js
@@ -0,0 +1,33 @@
+import NavBar from "../components/Navbar";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+function UserProfile() {
+  const [user, setUser] = useState({});
+  const params = useParams();
+  const userId = params.id;
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/users/${userId.id}`)
+      .then((response) => response.json())
+      .then((data) => setUser(data))
+      .catch((error) => console.log(error));
+  }, [userId]);
+
+  if (!user.name) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>{user.name}</h1>
+      </main>
+    </>
+  );
+}
+
+export default UserProfile;
